7e267416f8f58f6cd863eb0c7401071c1b029a62
shaked571 <shakedgreenfeld@gmail.com>
2018-11-17 20:29:06 +0200

    divide corpus

--- question_2.py
+++ question_2.py
@@ -1,3 +1,12 @@
 import nltk
+from nltk.corpus import brown
 
-nltk.download('brown')
+# nltk.download('brown')
+news_tagged = brown.tagged_sents(categories='news')  # Download all the data
+train_set = news_tagged._pieces[:(int)(len(news_tagged._pieces) * 0.9)]
+test_set = news_tagged._pieces[int(len(news_tagged._pieces) * 0.9):]
+print(len(train_set))
+print(len(test_set))
+
+for tag_sents in brown.tagged_sents():
+    print(tag_sents)
