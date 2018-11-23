93e82a4b2db816f4e368a4688424f80e38f99820
shaked571 <shakedgreenfeld@gmail.com>
2018-11-23 17:13:09 +0200

    start viterbi

--- viterbi.py
+++ viterbi.py
@@ -1,8 +1,12 @@
 import question_2
 
 
-table = [{}]
 my_train_data = question_2.get_word_tag_train_full_list()
 for couple in my_train_data:
+    print(couple)
 
 
+def viterbi_algorithm(observation, states, initial_prob, transition_matrix, emission_matrix):
+    table = [{}]
+    for state in states:
+        table[0][state] = {"prob": initial_prob[state] * emission_matrix[state][observation[0]], "prev": None}
