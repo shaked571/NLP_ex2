bc2c9a3c662aa5801faed7088ba24b30cc1a591d
shaked571 <shakedgreenfeld@gmail.com>
2018-11-23 16:13:46 +0200

    q_2 start

--- question_2.py
+++ question_2.py
@@ -1,13 +1,28 @@
 import nltk
 from nltk.corpus import brown
+import copy
 
 # nltk.download('brown')
 news_tagged = brown.tagged_sents(categories='news')  # Download all the data
 train_set = news_tagged._pieces[:(int)(len(news_tagged._pieces) * 0.9)]
 test_set = news_tagged._pieces[int(len(news_tagged._pieces) * 0.9):]
-print(len(train_set))
-print(len(test_set))
+# print(len(train_set))
+# print(len(test_set))
 
-# for tag_sents in brown.tagged_sents():
-#     print(tag_sents)
 
+def get_train_set():
+    return train_set
+
+
+def get_test_set():
+    return test_set
+
+
+def most_likely_tag():
+    my_train_set = copy.deepcopy(get_train_set())
+    for file in my_train_set:
+        for line in file:
+            for couple in line:
+                print(couple)
+
+most_likely_tag()
