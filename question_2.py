774e05a5eecbe67f951f967363f4a05173082724
shaked571 <shakedgreenfeld@gmail.com>
2018-11-23 15:34:23 +0200

    check 2

--- question_2.py
+++ question_2.py
@@ -8,9 +8,18 @@ test_set = news_tagged._pieces[int(len(news_tagged._pieces) * 0.9):]
 print(len(train_set))
 print(len(test_set))
 
-for tag_sents in brown.tagged_sents():
-    print(tag_sents)
+# for tag_sents in brown.tagged_sents():
+#     print(tag_sents)
+
 
 def foo():
     print('a')
+
+def shaked():
+    print('king')
+
+def most_likely_tag_baseline():
+    for word_and_tag in train_set:
+        print(word_and_tag)
+
 
