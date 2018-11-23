a9e6d73bc5f42d365b26ae0466badeaea11bd1ee
elad.bendavid <elad.bendavid1@cs.huji.ac.il>
2018-11-23 18:14:27 +0200

    word tag list

--- question_2.py
+++ question_2.py
@@ -18,6 +18,16 @@ def get_word_tag_full_list(corpus: list) -> list:
     return copy.deepcopy(word_tag_list)
 
 
+def get_word_full_list(corpus: list) -> set:
+    word_tag_list = get_word_tag_full_list(corpus)
+    return set([word_tag[0] for word_tag in word_tag_list])
+
+
+def get_tag_full_list(corpus: list) -> set:
+    word_tag_list = get_word_tag_full_list(corpus)
+    return set([word_tag[1] for word_tag in word_tag_list])
+
+
 def get_train_set() -> list:
     train_set = news_tagged._pieces[:(int(len(news_tagged._pieces) * 0.9))]
     return train_set
@@ -69,6 +79,22 @@ def most_likely_tag(train_set: list, test_set: list) -> (float, float, float):
     unknown_words_error_rate = unknown_words_prediction_wrong / (unknown_words_prediction_wrong + unknown_words_prediction_right)
     total_error_rate = (known_words_prediction_wrong + unknown_words_prediction_wrong) / len(test_set)
     return known_words_error_rate, unknown_words_error_rate, total_error_rate
+
+#
+# def train_bigram_hmm(corpus: list) ->dict():
+#     probability_map = dict()
+#     first_word, first_tag = corpus[0]
+#     next(corpus)
+#     for second_word, second_tag in corpus:
+#         if word in probability_map:
+#             if tag in probability_map[word]:
+#                 probability_map[word][tag] += 1
+#             else:
+#                 probability_map[word][tag] = 1
+#         else:
+#             probability_map[word] = dict()
+#             probability_map[word][tag] = 1
+#     return probability_map
 
 
 known_words_error, unknown_words_error, total_error = most_likely_tag(get_word_tag_full_list(get_train_set()), get_word_tag_full_list(get_test_set()))
